b5952d8a0942860c4513ce3d381f7452c20a12d7
John Paul Mathew Lacsamana <28johnpaul11@gmail.com>
2026-04-10 20:56:53 +0800

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
 // worker.js
+
 try {
     importScripts("./pyodide_engine/pyodide.js"); 
 } catch (e) {
@@ -6,6 +7,11 @@ try {
 }
 
 let pyodideEngine = null;
+
+// Expose a telemetry function to the Python environment
+self.sendTelemetry = (ops, peak_mem) => {
+    postMessage({ type: "TELEMETRY", ops: ops, mem: peak_mem });
+};
 
 async function loadPyodideEngine() {
     try {
@@ -35,6 +41,7 @@ MAX_OUTPUT_CHARS = 50000
 output_capture = io.StringIO()
 sys.stdout = output_capture
 
+# Input Automation
 input_counter = 0
 def automated_input(prompt=""):
     global input_counter
@@ -45,20 +52,58 @@ def automated_input(prompt=""):
 start_time = time.time()
 error_msg = ""
 final_ops = 0
+final_peak_mem = 0
 
 try:
     sys.setrecursionlimit(5000)
+    
+    proxy_definitions = """
+import js  # FIXED: Moved inside the proxy block so the exec() scope can see it
+
+def _check_telemetry():
+    # Stream data to the frontend every 250 operations
+    if __tracker['ops'] % 250 == 0:
+        js.sendTelemetry(__tracker['ops'], __tracker['peak_mem'])
+
+def _update_mem(bytes_added):
+    global __tracker
+    __tracker['current_mem'] += bytes_added
+    if __tracker['current_mem'] > __tracker['peak_mem']:
+        __tracker['peak_mem'] = __tracker['current_mem']
+
+class GreenList(list):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self._size = 56 + (len(self) * 8)
+        _update_mem(self._size)
+        
+    def append(self, item):
+        super().append(item)
+        _update_mem(8)
+        
+    def pop(self, index=-1):
+        if len(self) > 0:
+            _update_mem(-8)
+        return super().pop(index)
+        
+    def clear(self):
+        freed_bytes = len(self) * 8
+        super().clear()
+        _update_mem(-freed_bytes)
+"""
+
+    full_code = proxy_definitions + "\\n" + ${JSON.stringify(userCode)}
+    
     exec_globals = {
         'input': automated_input,
         '__name__': '__main__'
     }
     
-    # Execute the JS-instrumented code directly
-    exec(${JSON.stringify(userCode)}, exec_globals)
+    exec(full_code, exec_globals)
     
-    # Retrieve the dynamically tracked operations from the dictionary
     if '__tracker' in exec_globals:
-        final_ops = exec_globals['__tracker']['ops']
+        final_ops = exec_globals['__tracker'].get('ops', 0)
+        final_peak_mem = exec_globals['__tracker'].get('peak_mem', 0)
 
 except Exception as e:
     error_msg = str(e)
@@ -71,7 +116,7 @@ result = {
     "output": output_capture.getvalue()[:MAX_OUTPUT_CHARS],
     "error": error_msg,
     "ops": final_ops, 
-    "memory_peak_bytes": 0, # Placeholder until Proxy Objects are implemented
+    "memory_peak_bytes": final_peak_mem, 
     "duration_sec": end_time - start_time
 }
 json.dumps(result)
